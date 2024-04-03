7def6a00b3e25e391388f288ba4e1efadfd31ed2
justin <justinmwathi32@gmail.com>
2024-04-03 20:10:04 +0300

    Lab completed

--- main.js
+++ main.js
@@ -3,6 +3,82 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const heart = document.querySelector('.like-glyph');
+
+const errorModal = document.getElementById('modal');
+
+const errorMessage = document.querySelector('#modal-message');
+
+
+// Add the .hidden class to the error modal in the HTML so it does not appear when the page first loads
+
+errorModal.classList.add('hidden');
+
+
+heart.addEventListener('click', () => {
+
+  if (heart.classList.contains('activated-heart')) {
+
+    // When a user clicks on a full heart
+
+    heart.classList.remove('activated-heart');
+
+    heart.classList.add('empty');
+
+  } else {
+
+    // When a user clicks on an empty heart
+
+    mimicServerCall()
+
+      .then((response) => {
+
+        if (response.success) {
+
+          // When the "server" returns a success status
+
+          heart.classList.remove('empty');
+
+          heart.classList.add('activated-heart');
+
+        } else {
+
+          // When the "server" returns a failure status
+
+          errorModal.classList.remove('hidden');
+
+          errorMessage.textContent = response.message;
+
+          setTimeout(() => {
+
+            errorModal.classList.add('hidden');
+
+          }, 3000);
+
+        }
+
+      })
+
+      .catch(() => {
+
+        // Respond to the error using a .catch(() => {}) block after your .then(() => {}) block.
+
+        errorModal.classList.remove('hidden');
+
+        errorMessage.textContent = 'An error occurred. Please try again later.';
+
+        setTimeout(() => {
+
+          errorModal.classList.add('hidden');
+
+        }, 3000);
+
+      });
+
+  }
+
+});
+
 
 
 
